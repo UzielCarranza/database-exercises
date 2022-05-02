f3110173fdcb5ea8c6604122d0f31ab5b648d5d5
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-02 12:35:11 -0400

    created table albums within codeup_test_db database

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,4 +1,15 @@
 USE codeup_test_db;
 
 
-DROP TABLE IF EXISTS albums;
+# DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums
+(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    artist VARCHAR(255) not null,
+    name VARCHAR(255) not null,
+    release_date INT UNSIGNED CHECK ( release_date between 1400 and 3000),
+    sales FLOAT UNSIGNED,
+    genre VARCHAR(255) not null
+)
+
