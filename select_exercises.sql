f35a7e24dc2088c0a11683459abdd0b03a30bab3
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-02 15:47:52 -0400

    getting albums by release date 1990

--- select_exercises.sql
+++ select_exercises.sql
@@ -7,3 +7,11 @@ WHERE artist = 'Pink Floyd';
 SELECT release_date AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was released'
 FROM albums
 WHERE name LIKE 'Sgt%';
+
+SELECT genre AS 'The genre for Nevermind'
+FROM albums
+WHERE name = 'Nevermind';
+
+SELECT name AS 'Which albums were released in the 1990s'
+FROM albums
+WHERE release_date = 1990;
