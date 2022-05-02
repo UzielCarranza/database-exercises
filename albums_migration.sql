be7557c38a56a6a882ad211da61fde591518dc73
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-02 13:18:21 -0400

    fixed release date to not allow null data

--- albums_migration.sql
+++ albums_migration.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS albums
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     artist VARCHAR(255) not null,
     name VARCHAR(255) not null,
-    release_date INT UNSIGNED CHECK ( release_date between 1400 and 3000),
-    sales FLOAT UNSIGNED,
-    genre VARCHAR(255) not null
+    release_date INT UNSIGNED NOT NULL CHECK ( release_date between 1400 and 3000),
+    sales FLOAT UNSIGNED NOT NULL,
+    genre VARCHAR(255)
 );
 
 USE codeup_test_db;
