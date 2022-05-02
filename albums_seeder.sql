18735bf6649a3315969a4c9be32e07fdbb6eda6c
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-02 15:47:28 -0400

    uncommented

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,16 +1,16 @@
 USE codeup_test_db;
 
-# INSERT INTO albums(artist, name, release_date, sales, genre)
-# VALUES('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop, post-disco, funk, rock'),
-#       ('AC/DC', 'Back in Black', 1980, 29.5, 'Hard rock'),
-#       ('Whitney Houston', 'The Bodyguard', 1992, 32.4, 'R&B, soul, pop, soundtrack'),
-#       ('Meat Loaf', 'Bat Out of Hell', 1977, 21.7, 'Hard rock, glam rock, progressive rock'),
-#       ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 41.2, 'Country rock, soft rock, folk rock'),
-#       ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.4, 'Progressive rock'),
-#       ('Eagles', 'Hotel California', 1976, 31.5, 'Soft rock'),
-#       ('Bee Gees ', 'Saturday Night Fever', 1977, 21.6, 'Disco'),
-#       ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock'),
-#       ('Shania Twain', 'Come On Over', 1997, 29.6, 'Country, pop');
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 50.2, 'Pop, post-disco, funk, rock'),
+       ('AC/DC', 'Back in Black', 1980, 29.5, 'Hard rock'),
+       ('Whitney Houston', 'The Bodyguard', 1992, 32.4, 'R&B, soul, pop, soundtrack'),
+       ('Meat Loaf', 'Bat Out of Hell', 1977, 21.7, 'Hard rock, glam rock, progressive rock'),
+       ('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 41.2, 'Country rock, soft rock, folk rock'),
+       ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.4, 'Progressive rock'),
+       ('Eagles', 'Hotel California', 1976, 31.5, 'Soft rock'),
+       ('Bee Gees ', 'Saturday Night Fever', 1977, 21.6, 'Disco'),
+       ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock'),
+       ('Shania Twain', 'Come On Over', 1997, 29.6, 'Country, pop');
 
 
 
