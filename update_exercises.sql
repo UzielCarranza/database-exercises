dce3b778a0cc10c60ff5e0501d956d1e9d0497a3
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-02 16:10:54 -0400

    changed release date that is < 1980 to be 1980

--- update_exercises.sql
+++ update_exercises.sql
@@ -19,6 +19,19 @@ UPDATE albums
 SET sales = sales * 10;
 
 
-SELECT sales AS 'ALL albums'
+SELECT sales AS 'All albums changed * 10'
 FROM albums
 WHERE artist = 'Adele';
+
+
+
+UPDATE albums
+SET release_date = 1800
+WHERE release_date < 1980;
+
+
+SELECT release_date AS 'Release date change to 1980'
+FROM albums
+WHERE release_date = 1800;
+
+
