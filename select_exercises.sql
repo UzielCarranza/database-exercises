5db366353448ac57caee432984c98845fd430f17
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-02 15:44:12 -0400

    getting specific album's name release date

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,4 +2,8 @@ USE codeup_test_db;
 
 SELECT name AS 'The name of all albums by Pink Floyd'
 FROM albums
-WHERE artist = 'Pink Floyd'
+WHERE artist = 'Pink Floyd';
+
+SELECT release_date AS 'The year Sgt. Pepper''s Lonely Hearts Club Band was released'
+FROM albums
+WHERE name LIKE 'Sgt%';
