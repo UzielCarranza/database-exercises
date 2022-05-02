5611fea8fc433a062b705bf1673c29d1c5c70a74
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-02 12:16:26 -0400

    created examples exercises to create databases

---
+++ tables.sql
@@ -0,0 +1,22 @@
+SHOW DATABASES;
+
+USE codeup_test_db;
+CREATE TABLE IF NOT EXISTS users
+(
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username    VARCHAR(100) NOT NULL,
+    email       VARCHAR(255) NOT NULL,
+    first_name  VARCHAR(255) NOT NULL,
+    last_name   VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+
+CREATE  TABLE IF NOT EXISTS cats
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    bread VARCHAR(255) NOT NULL,
+    age INT UNSIGNED NOT NULL,
+    name VARCHAR(255)
+);
