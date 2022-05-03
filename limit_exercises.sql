e41c499a2acbedc4006d83c06e5ea4e9b193640c
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-03 15:11:06 -0400

    completed exercises

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -4,6 +4,12 @@ SELECT emp_no FROM employees.salaries
 ORDER BY salary DESC
 LIMIT 5;
 
+SELECT DISTINCT e.last_name
+FROM employees e
+ORDER BY e.last_name DESC
+LIMIT 10;
+
+
 SELECT emp_no FROM employees.salaries
 ORDER BY salary DESC
 LIMIT 4 OFFSET 46;
