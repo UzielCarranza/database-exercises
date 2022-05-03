42577a1bc2d6a7b2cd1cb546b54065c9d042ed70
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-03 11:35:36 -0400

    using OR statement to find employees with a certain name

--- where_exercises.sql
+++ where_exercises.sql
@@ -10,3 +10,9 @@ WHERE last_name LIKE 'E%';
 
 SELECT last_name FROM employees
 WHERE last_name LIKE '%q%';
+
+
+# SECOND PART
+
+SELECT first_name FROM employees
+WHERE first_name = 'IRENA' OR first_name = 'Vidya' OR first_name = 'Maya';
