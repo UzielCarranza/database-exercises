da5a1ea45db602370d6f44877c458fb9eddfaab6
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-03 11:48:30 -0400

    copying contents from where_exercises to modify them to use order by

---
+++ order_by_exercises.sql
@@ -0,0 +1,39 @@
+USE employees;
+
+SELECT first_name FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+
+SELECT first_name FROM employees
+WHERE last_name LIKE 'E%';
+
+
+SELECT last_name FROM employees
+WHERE last_name LIKE '%q%';
+
+
+# SECOND PART
+
+SELECT first_name FROM employees
+WHERE first_name = 'IRENA' OR first_name = 'Vidya' OR first_name = 'Maya';
+
+
+
+SELECT first_name FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+  AND gender = 'M';
+
+
+SELECT last_name FROM employees
+WHERE last_name LIKE '%E'
+   OR last_name LIKE 'E%';
+
+
+SELECT last_name FROM employees
+WHERE last_name LIKE '%E'
+  AND last_name LIKE 'E%';
+
+
+SELECT last_name FROM employees
+WHERE NOT last_name LIKE '%qu%'
+  AND last_name LIKE '%q%';
