77f53585f3aa991caeb44335a4b044449ba03790
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-03 19:55:37 -0400

    quering number of assoiates with specific job title

--- join_test_db.sql
+++ join_test_db.sql
@@ -19,3 +19,10 @@ FROM users
 SELECT users.name AS user_name, roles.name AS role_name
 FROM users
          RIGHT JOIN roles ON users.role_id = roles.id;
+
+
+
+SELECT COUNT(users.name) as Number_of_User, roles.name as role_name
+FROM users
+         JOIN roles ON users.role_id = roles.id
+GROUP BY roles.name;
