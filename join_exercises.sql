2a8d5157a1cc3eaa9d1d1d101a85b583f159af17
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-04 10:40:30 -0400

    completed exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,7 +1,7 @@
 USE employees;
 
 
-SELECT d.dept_name,  CONCAT(m.first_name, ' ', m.last_name) AS department_manager
+SELECT d.dept_name, CONCAT(m.first_name, ' ', m.last_name) AS department_manager
 FROM employees as m
          JOIN dept_manager as de
               ON de.emp_no = m.emp_no
@@ -20,4 +20,45 @@ FROM departments d
               on e.emp_no = dm.emp_no
 WHERE to_date LIKE '9%'
 ORDER BY dept_name;
+
+
+
+USE employees;
+
+
+SELECT d.dept_name, CONCAT(m.first_name, ' ', m.last_name) AS department_manager_Women
+FROM employees as m
+         JOIN dept_manager as de
+              ON de.emp_no = m.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE gender = 'F'
+  AND to_date LIKE '9%'
+ORDER BY dept_name;
+
+
+
+SELECT title, COUNT(title) AS total
+FROM titles t
+         JOIN dept_emp de
+              ON t.emp_no = de.emp_no
+WHERE dept_no = 'd009'
+  AND t.to_date LIKE '9%'
+  AND de.to_date LIKE '9%'
+GROUP BY title;
+
+
+SELECT dept_name                          AS department,
+       CONCAT(first_name, ' ', last_name) AS Full_Name,
+       salary                             AS salary
+FROM employees e
+         JOIN dept_manager as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+         JOIN salaries s on e.emp_no = s.emp_no
+WHERE de.to_date LIKE '9%'
+  AND s.to_date LIKE '9%'
+ORDER BY dept_name;
+
 
