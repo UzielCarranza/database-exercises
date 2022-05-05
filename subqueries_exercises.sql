4a2cf5bedaf267392a40446cb28fc2e742377f3a
Uziel Carranza Charro <uzielcarranzac97@gmail.com>
2022-05-05 11:54:17 -0400

    Finding all the titles held by all employees with the first name Aamod, using a subquery

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -9,4 +9,18 @@ WHERE hire_date IN (
     SELECT hire_date
     FROM employees
     WHERE employees.employees.emp_no = 101010
+);
+
+
+# Find all the titles held by all employees with the first name Aamod.
+# 314 total titles, 6 unique titles
+
+
+SELECT t.title AS Title, COUNT(t.title) AS titles_hold_by_employees_Aamod
+FROM titles t
+WHERE emp_no IN (
+    SELECT e.emp_no
+    FROM employees e
+    WHERE first_name LIKE 'Aamod'
 )
+GROUP BY title;
